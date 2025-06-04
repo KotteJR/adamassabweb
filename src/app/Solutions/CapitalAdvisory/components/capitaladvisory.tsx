3a2e6caaf4c839784d988e7fe356dfab7e10e0f6
KotteJR <aleksandar.kotevski@adamass.se>
2025-06-04 16:13:23 +0200

    FIXES?

--- src/app/Solutions/CapitalAdvisory/components/capitaladvisory.tsx
+++ src/app/Solutions/CapitalAdvisory/components/capitaladvisory.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 const stats = [
   {
